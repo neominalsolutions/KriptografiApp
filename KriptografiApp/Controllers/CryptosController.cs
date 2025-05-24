ce23bd8cc91d06fc6a87fce95168d4fe353f0c54
neominal.solutions <neominal.solutions@gmail.com>
2025-05-24 10:31:02 +0300

    Add project files.

---
+++ KriptografiApp/Controllers/CryptosController.cs
@@ -0,0 +1,53 @@
+﻿using KriptografiApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KriptografiApp.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CryptosController : ControllerBase
+  {
+    private readonly RSAService _rSAService;
+
+
+    public CryptosController(RSAService rSAService)
+    {
+      _rSAService = rSAService;
+    }
+
+    // Public Key Base64 göndermek
+    [HttpGet("publicKey")]
+    public IActionResult GetPublicKey()
+    {
+      var publicKey = System.IO.File.ReadAllText("RsaKeys/public_key1.pem");
+
+      // Temizleme işlemini Pem dosyasını Base64 çeviridğimizden yaptık.
+      string base64PublicKey = publicKey.Replace("\n", "").Replace("\r","").Replace("-----BEGIN PUBLIC KEY-----", "").Replace("-----END PUBLIC KEY-----", "").Trim();
+
+      var keyBytesBase64 = Convert.FromBase64String(base64PublicKey);
+
+
+      return Ok(new { keyBytesBase64 });
+    }
+
+
+    [HttpPost("rsaDecrypt")]
+    public IActionResult RsaDecrypt(string ciperText)
+    {
+      var data =  _rSAService.Decrypt(ciperText);
+
+      return Ok(new { data });
+    }
+
+
+    [HttpPost("rsaEncrpt")]
+    public IActionResult RsaEncrpt(string plainText, string publicKeyBase64)
+    {
+      var response = _rSAService.Encrypt(plainText, publicKeyBase64);
+
+
+      return Ok(new {response});
+    }
+  }
+}
---
+++ KriptografiApp/Services/RSAService.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace KriptografiApp.Services
+{
+  public class RSAService
+  {
+    private readonly RSA _privateRsa;
+    private readonly RSA _publicRsa;
+
+
+        public RSAService()
+        {
+            _privateRsa = RSA.Create();
+            _publicRsa = RSA.Create();
+      // Load the keys from files
+          var privateKey = File.ReadAllText("RsaKeys/private_key.pem");
+          _privateRsa.ImportFromPem(privateKey.ToCharArray());
+        }
+
+    // encrypt işlemi için public key kullanıyoruz
+    public string Encrypt(string plainText, string publicKeyBase64)
+    {
+      byte[] bytes = Convert.FromBase64String(publicKeyBase64);
+      // hangi public key ile encrypt edildiğini anlamak için public key ile decrypt etmeye çalışıyoruz
+      _publicRsa.ImportSubjectPublicKeyInfo(bytes, out _);
+
+      var data = System.Text.Encoding.UTF8.GetBytes(plainText);
+      var encryptedData = _publicRsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+
+
+      return Convert.ToBase64String(encryptedData);
+    }
+
+    // Not: Bu metot sadece private key ile decrypt edilebilir. decrypt işlemleri için private key kullanalım.
+    public string Decrypt(string encrypted)
+    {
+
+      var data = Convert.FromBase64String(encrypted);
+      var encryptedData = _privateRsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+
+      return Encoding.UTF8.GetString(encryptedData);
+    }
+
+  }
+
+
+}
